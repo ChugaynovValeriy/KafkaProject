664a47c7ce72121dcd4dbc78e4d6b51cae440f40
Valeriy <chugajnov.va@edu.spbstu.ru>
2022-07-18 02:16:06 +0300

    Initial kafka app

---
+++ src/main/java/study/controller/MessageController.java
@@ -0,0 +1,25 @@
+package study.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public MessageController(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+
+    @PostMapping
+    public void sendOrder(String messageId, String message) {
+        kafkaTemplate.send("messageTopic", messageId, message);
+    }
+}
